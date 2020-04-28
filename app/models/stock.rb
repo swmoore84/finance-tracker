7876183bfe385a30e1b73102a25821011da45670
Sam Moore <swmoore84@gmail.com>
2020-04-28 22:48:30 +0900

    added error handling for search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,6 +6,10 @@ class Stock < ApplicationRecord
       secret_token: 'secret_token',
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
