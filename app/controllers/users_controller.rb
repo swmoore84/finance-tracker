044e7d778f2c03133edff08622c0878ce6cf842b
Sam Moore <swmoore84@gmail.com>
2020-05-04 23:45:19 +0900

    added stocks table to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def search
